dceb3496d76937576bd1aa7b2ed109cf8c7a571c
Cliff-Ezra <cliffezra212@gmail.com>
2024-03-08 08:38:25 +0300

    `feat:` footer page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,18 +21,16 @@ const Footer = () => {
       </div>
       <div className="footer-section-two">
         <div className="footer-section-columns">
-          <span>Qualtiy</span>
-          <span>Help</span>
-          <span>Share</span>
-          <span>Carrers</span>
+          <span>About Us</span>
+          <span>Explore</span>
+          <span>FAQ</span>
           <span>Testimonials</span>
-          <span>Work</span>
+          <span>Contact</span>
         </div>
         <div className="footer-section-columns">
-          <span>244-5333-7783</span>
-          <span>hello@food.com</span>
-          <span>press@food.com</span>
-          <span>contact@food.com</span>
+          <span>+256-775-926572</span>
+          <span>dr@dealroom.com</span>
+          <span>veritas@veritas-interactive.com</span>
         </div>
         <div className="footer-section-columns">
           <span>Terms & Conditions</span>
